17ac389331bcba3cf4adb9ab4af83fe3cd2906b8
koreyawashington <122809994+koreyawashington@users.noreply.github.com>
2023-04-30 12:36:54 -0400

    renderingnotes

--- notesapp/src/App.js
+++ notesapp/src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import axios from 'axios'
 
 function App() {
+  //created states to store our notes
+  const [noteapp, setNoteApp] = useState(null)
+  //the useEffect function will make start running our function one time as soon as the application is open in the url
+  useEffect(() => {
+    //inspect the console in the REACT App to confirm that 
+    getNotes()
+   //leaving the square bracket empty will ensure the function runs once when the application is opened in the url 
+  }, [])
+  //created function to fetch the notes from localhost:3000/notes(my backend server)
+  const getNotes = async () => {
+    //fetch notes using axios
+    const response = axios.get('http://localhost:3000/notes')
+    //set to state
+    //console.log the response and view the data to see the notes the noteapp title and body
+    console.log(response);
+    setNoteApp((await response).data.notes)
+    console.log(response);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div>
+      <h2>NOTES:</h2>
+      {noteapp && noteapp.map(note => {
+        return (
+          //when looping through an array and rendering it each child has to have a unique key to be rendered in our app
+          <div key={note._id}>
+            <h3>{note.title}</h3>
+            </div>
+        )
+      } )}
+     </div>
+
+     {/* I have added a form to create a note or notes */}
+     <div>
+      <h2>Create Note</h2>
+     </div>
     </div>
   );
 }
